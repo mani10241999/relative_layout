d8f5375467c2a4ac6eb0b6ba153ee894bbb1f0f8
mani869081 <mani869081@gamil.com>
2022-03-27 20:48:12 +0530

    Relative_layout

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
-import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+
+
+@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relativelayout);
-
-
+        setContentView(R.layout.activity_main);
 
 
     }
